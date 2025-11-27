6690deb246affd9dd904a421921ee2344fcdf777
LiPei <peggy931001@gmail.com>
2025-11-28 03:01:43 +0800

    Fix lint error

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const unusedVariable = 'this will cause lint error';  // 加這行
-
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from LSAP CI/CD App!' });
 });
